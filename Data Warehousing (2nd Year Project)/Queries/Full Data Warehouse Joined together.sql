17b0f56648824af4fc91107c7f467c89bf850c9b
Nathan Crone <nathan.crone2@mail.dcu.ie>
2021-01-10 10:08:07 +0000

    Remove project from this repo

--- Data Warehousing (2nd Year Project)/Queries/Full Data Warehouse Joined together.sql
+++
@@ -1,8 +0,0 @@
-SELECT *
-FROM fact_table JOIN residence USING (Eir_code)
-				JOIN school USING (School_roll_number)
-				JOIN student USING (Exam_number)
-				JOIN subject USING (Subject_code)
-				JOIN time USING (Year)
-                
-GROUP BY School_roll_number
